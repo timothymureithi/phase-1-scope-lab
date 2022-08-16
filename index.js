fcde749efda918c57ee7d7e580bbc683d4034f20
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-08-16 19:38:46 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+//use var
+var customerName = 'bob';
+
+
+//uppercase
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+//setBestCustomer()
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+//overwriteBestCustomer() -> leastFavoriteCustomer
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+
+}
+const leastFavoriteCustomer = 'Bob\'s brother';
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Bob\'s sister';
+}
